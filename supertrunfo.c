7e94541acbe249aefc69f003207bbc110fdcdb27
Gaby1876 <rgabryella581@gmail.com>
2025-03-13 19:54:20 +0000

    SuperTrunfo CORRIGIDO

--- supertrunfo.c
+++ supertrunfo.c
@@ -2,80 +2,80 @@
 #include <string.h>
 
 int main() {
-    char estado1[50], estado2[50]; strcpy(estado1, "SP"); strcpy(estado2, "RJ");
-    char código1[30], código2[50]; strcpy(código1, "A01"); strcpy(código2, "B02");
-    char cidade1[50], cidade2[50]; strcpy(cidade1, "São Paulo"); strcpy(cidade2, "Rio de Janeiro");
-    int população1, população2; população1 = 12325000; população2 = 6748000;
-    float área1, área2; área1 = 1521.11; área2 =  1200.25;
-    float pib1, pib2; pib1 =  699.28 ; pib2 = 300.50;
-    int pontos_turísticos1, pontos_turísticos2; pontos_turísticos1 = 50; pontos_turísticos2 = 30;
+    char estado1[50], estado2[50];
+    char codigo1[30], codigo2[50];
+    char cidade1[50], cidade2[50];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontos_turisticos1, pontos_turisticos2;
 
-//Carta 01
+    //Carta 01
+    printf("Digite a sigla do Estado (ex: SP):\n");
+    scanf(" %49s", estado1);
 
-printf("Digite a sigla do Estado ( ex: SP):\n");
-scanf(" %2s", strcpy(estado1, "SP"));
+    printf("Digite o codigo da carta (ex: A01):\n");
+    scanf(" %29s", codigo1);
 
-printf("Digite o código da carta (ex: A01):\n");
-scanf(" %s", strcpy(código1, "A01"));
+    printf("Digite o nome da cidade:\n");
+    scanf(" %49s", cidade1);
 
-//saída de dados
+    printf("Digite a populacao:\n");
+    scanf(" %d", &populacao1);
 
+    printf("Digite a area em km2:\n");
+    scanf(" %f", &area1);
 
-printf("A cidade é: %s\n", cidade1);
+    printf("Digite o PIB em bilhoes de reais:\n");
+    scanf(" %f", &pib1);
 
-
-printf("População:%d\n", população1);
-
-
-printf("Área em km2:%f\n", área1);
-
-printf("PIB:%f bilhões de reais\n", pib1);
-
-
-printf("Pontos turísticos:%d\n", pontos_turísticos1);
+    printf("Digite o numero de pontos turisticos:\n");
+    scanf(" %d", &pontos_turisticos1);
 
 
-//Carta 02
+    //Carta 02
+    printf("Digite a sigla do Estado:\n");
+    scanf(" %49s", estado2);
 
-printf("Digite a sigla do Estado\n");
-scanf(" %s", strcpy(estado2, "RJ"));
+    printf("Digite o codigo da carta:\n");
+    scanf(" %49s", codigo2);
 
-printf("Digite o código da carta\n");
-scanf(" %s", strcpy(código2, "B02"));
+    printf("Digite o nome da cidade:\n");
+    scanf(" %49s", cidade2);
 
-// saída de dados
-printf("A cidade é:%s\n", cidade2);
+    printf("Digite a populacao:\n");
+    scanf(" %d", &populacao2);
 
-printf("População:%d\n", população2);
+    printf("Digite a area em km2:\n");
+    scanf(" %f", &area2);
+
+    printf("Digite o PIB em bilhoes de reais:\n");
+    scanf(" %f", &pib2);
+
+    printf("Digite o numero de pontos turisticos:\n");
+    scanf(" %d", &pontos_turisticos2);
 
 
-printf("Área em km2:%f\n", área2);
+    //saída de dados - carta 1
+    printf("\n--- Carta 1 ---\n");
+    printf("Estado: %s\n", estado1);
+    printf("Codigo: %s\n", codigo1);
+    printf("Cidade: %s\n", cidade1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %f\n", area1);
+    printf("PIB: %f\n", pib1);
+    printf("Pontos turisticos: %d\n", pontos_turisticos1);
 
-printf("PIB:%fbilhões de reais\n", pib2);
+    //saída de dados - carta 2
+    printf("\n--- Carta 2 ---\n");
+    printf("Estado: %s\n", estado2);
+    printf("Codigo: %s\n", codigo2);
+    printf("Cidade: %s\n", cidade2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %f\n", area2);
+    printf("PIB: %f\n", pib2);
+    printf("Pontos turisticos: %d\n", pontos_turisticos2);
+ 
+    return 0;
 
-
-printf("Pontos Turísticos:%d\n", pontos_turísticos2);
-
-//carta 1
-printf("carta 1\n");
-printf("Estado: %s\n", estado1);
-printf("Código: %s\n", código1);
-printf("Cidade: %s\n", cidade1);
-printf("População: %d\n", população1);
-printf("Área: %f\n", área1);
-printf("PIB: %f\n", pib1);
-printf("Pontos turísticos: %d\n", pontos_turísticos1);
-
-//carta 2
-
-printf("carta 2\n");
-printf("Estado: %s\n", estado2);
-printf("Código: %s\n", código2);
-printf("Cidade: %s\n", cidade2);
-printf("População: %d\n", população2);
-printf("Área: %f\n", área2);
-printf("PIB: %f\n", pib2);
-printf("Pontos turísticos: %d\n", pontos_turísticos2);
-
-	return 0;
 }
